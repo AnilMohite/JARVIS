e21599b4648ff8b75bf60d5056a2b832a2f22a90
anilmohite22 <anil.mohite@crayoninfotech.com>
2023-11-24 21:02:21 +0530

    add ai model and pass trainging data

--- jarvis_new.py
+++ jarvis_new.py
@@ -1,8 +1,16 @@
 import pyttsx3
 import speech_recognition as sr
-import random
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.svm import SVC
+import nltk
+from sklearn.model_selection import train_test_split
+import json
 import warnings
 warnings.simplefilter('ignore')
+import random
+
+# first download nltk requied data 
+# nltk.download("punkt")
 
 def speak(text):
     engine = pyttsx3.init()
@@ -43,10 +51,53 @@ def mainExecution(query):
         time = datetime.now().strftime("%I:%M %p")
         speak(f"The time now is  : {time}")
 
-print("")
-print("==> Jarvis AI: Hello! How can I assist you?")
-print("")
+# print("")
+# print("==> Jarvis AI: Hello! How can I assist you?")
+# print("")
+# while True:
+#     # query = str(input("Enter Query: "))
+#     query = speechrecognition()
+#     mainExecution(query)
+
+# training data 
+with open('./training_data.json','r') as f:
+    intents = json.load(f)
+# print(intents)
+training_data = []
+labels = []
+
+for intent , data in intents.items():
+    for pattern in data['patterns']:
+        training_data.append(pattern.lower())
+        labels.append(intent)
+
+Vectorizer = TfidfVectorizer(tokenizer=nltk.word_tokenize,stop_words="english",max_df=0.8,min_df=1)
+X_train = Vectorizer.fit_transform(training_data)
+X_train,X_test,Y_train,Y_test = train_test_split(X_train,labels,test_size=0.4,random_state=42,stratify=labels)
+
+model = SVC(kernel='linear', probability=True, C=1.0)
+model.fit(X_train, Y_train)
+
+predictions = model.predict(X_test)
+
+def predict_intent(user_input):
+    user_input = user_input.lower()
+    input_vector = Vectorizer.transform([user_input])
+    intent = model.predict(input_vector)[0]
+    return intent
+
+print("AI Assistant: Hello! How can I assist you?")
 while True:
-    # query = str(input("Enter Query: "))
-    query = speechrecognition()
-    mainExecution(query)
+    user_input = speechrecognition()
+    if user_input.lower() == 'exit':
+        print("AI Assistant: Goodbye!")
+        break
+
+    intent = predict_intent(user_input)
+    if intent in intents:
+        responses = intents[intent]['responses']
+        response = random.choice(responses)
+        speak(response)
+
+    else:
+        speak("AI Assistant: Sorry, I'm not sure how to respond to that.")
